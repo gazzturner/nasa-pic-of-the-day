444abae067a7d0b5ff6930e85e11189771746c3a
Garry Turner <garry@evolving.technology>
2021-10-12 14:43:23 +0100

    initial commit

---
+++ AngularWebApp/scripts/app.js
@@ -0,0 +1,22 @@
+﻿var myApp = angular.module("app", []);
+
+var testController = function ($scope, $http) {
+    $http({
+        method: "Get", url: 'https://api.nasa.gov/planetary/apod?api_key=LTGJVdNUgkCcAPUeZDjdE9yAlDlh5atQVRGL4UJA'
+    }).then(function (response) {
+        var data = response.data;
+        $scope.date = data.date;
+        $scope.title = data.title;
+        $scope.picUrl = data.hdurl;
+        $scope.copyright = data.copyright;
+        $scope.description = data.explanation;
+    }, function (reason) {
+        $scope.error = reason.statusText;
+        });
+
+    $scope.redirect = function() {
+        window.open($scope.picUrl);
+    }
+}
+
+myApp.controller("testController", testController);
